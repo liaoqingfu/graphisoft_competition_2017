3e72e603d9ae307f5d838951a4b2667de272372c
Gabor Marton <martongabesz@gmail.com>
2017-04-05 20:16:05 +0200

    Remove parens

--- src/iskolabusz/a5.cpp
+++ src/iskolabusz/a5.cpp
@@ -3,5 +3,5 @@
 int main() {
     int n;
     std::cin >> n;
-    std::cout << ((n & (n - 1)) ? (n - (1<<(31 - __builtin_clz(n)))) * 2 : n);
+    std::cout << (n & n - 1 ? 2 * n - (2<<31 - __builtin_clz(n)) : n);
 }
