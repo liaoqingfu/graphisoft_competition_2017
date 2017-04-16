122f361f8d2667b81d4311b39b01d178ddbe1223
Peter Szabados <kangirigungi@gmail.com>
2017-04-16 18:16:24 +0200

    Add createOutput()

--- src/semifinal/track/GameState.hpp
+++ src/semifinal/track/GameState.hpp
@@ -18,5 +18,8 @@ struct GameState {
 GameState parseInitialGameState(const std::vector<std::string>& input); // TODO
 void parseTickInfo(GameState& gameState,
         const std::vector<std::string>& input); // TODO
+std::vector<std::string> createOutput(
+        Direction pushDirection, int pushPosition, int pushFieldType,
+        Point princessTarget); // TODO
 
 #endif // SEMIFINAL_TRACK_GAMESTATE_HPP
