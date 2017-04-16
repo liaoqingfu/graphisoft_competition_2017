d518ce29bc7a6faf49222ce722d9fffb48e3f380
Peter Szabados <kangirigungi@gmail.com>
2017-04-16 18:18:04 +0200

    Add Step

--- src/semifinal/track/GameState.hpp
+++ src/semifinal/track/GameState.hpp
@@ -15,11 +15,16 @@ struct GameState {
     int extraField;
 };
 
+struct Step {
+    Direction pushDirection;
+    int pushPosition;
+    int pushFieldType;
+    Point princessTarget;
+};
+
 GameState parseInitialGameState(const std::vector<std::string>& input); // TODO
 void parseTickInfo(GameState& gameState,
         const std::vector<std::string>& input); // TODO
-std::vector<std::string> createOutput(
-        Direction pushDirection, int pushPosition, int pushFieldType,
-        Point princessTarget); // TODO
+std::vector<std::string> createOutput(const Step& step); // TODO
 
 #endif // SEMIFINAL_TRACK_GAMESTATE_HPP
