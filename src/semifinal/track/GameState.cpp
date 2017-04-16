a21b92940b2b1f6775cb7b23d830ad42ca3edb28
Gabor Marton <martongabesz@gmail.com>
2017-04-16 20:16:01 +0200

    Add hook for GameState parsers

---
+++ src/semifinal/track/GameState.cpp
@@ -0,0 +1,11 @@
+#include "GameState.hpp"
+
+GameState parseInitialGameState(const std::vector<std::string>& input) {
+    return {};
+}
+
+void parseTickInfo(GameState& gameState,
+                   const std::vector<std::string>& input) {}
+
+std::vector<std::string> createOutput(const Step& step) { return {}; }
+
