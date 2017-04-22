79f0cbfc259ce9685ecd9dd8ee4804e3994e22be
Gabor Marton <martongabesz@gmail.com>
2017-04-22 12:09:59 +0200

    Add user and pw to random_client

--- src/semifinal/random_client/main.cpp
+++ src/semifinal/random_client/main.cpp
@@ -8,16 +8,16 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    if (argc < 6) {
+    if (argc < 4) {
         std::cerr << "Usage: " << argv[0]
-                << " host port team_name password task_id [seed]";
+                << " host port task_id [seed]";
     }
     const char* host_name = argv[1];
     const auto port = boost::lexical_cast<unsigned short>(argv[2]);
-    const char* team_name = argv[3];
-    const char* password = argv[4];
+    const int task_id = boost::lexical_cast<int>(argv[3]);
+    const char* team_name = "elvont_gyar";
+    const char* password = "T&kX,!RT;vXK";
 
-    const int task_id = boost::lexical_cast<int>(argv[5]);
 
     try {
         platform_dep::enable_socket _;
@@ -25,8 +25,8 @@ int main(int argc, char** argv) {
         using Solver = GenericSolver<ChoosingStrategy>;
 
         unsigned int seed;
-        if (argc > 6) {
-            seed = boost::lexical_cast<int>(argv[6]);
+        if (argc > 4) {
+            seed = boost::lexical_cast<int>(argv[4]);
         } else {
             seed = std::random_device{}();
         }
