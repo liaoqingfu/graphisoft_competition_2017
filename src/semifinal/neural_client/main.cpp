fbbb99644b60a898389f6469c66fab0195096301
András Németh <andrasnemeth@users.noreply.github.com>
2017-04-29 23:44:17 +0200

    Add neural client

---
+++ src/semifinal/neural_client/main.cpp
@@ -0,0 +1,86 @@
+#include "client.hpp"
+#include "ChoosingStrategy.hpp"
+#include "GenericSolver.hpp"
+#include "StrategyParser.hpp"
+
+#include <boost/lexical_cast.hpp>
+#include <boost/program_options.hpp>
+
+#include <iostream>
+#include <memory>
+#include <random>
+
+struct Options {
+    std::string hostname = "localhost";
+    unsigned short port = 32323;
+    int level = 1;
+    std::string teamName = "elvont_gyar";
+    std::string password = "T&kX,!RT;vXK";
+    unsigned seed = 0;
+    std::string strategyString = "";
+};
+
+namespace po = boost::program_options;
+
+template <typename T>
+po::typed_value<T>* defaultValue(T& v) {
+    return po::value(&v)->default_value(v);
+}
+
+
+Options parseOptions(int argc, const char* argv[]) {
+    po::options_description description;
+    Options options;
+    bool isHelpRequested = false;
+    description.add_options()
+            ("help,h", po::bool_switch(&isHelpRequested))
+            ("port,P", defaultValue(options.port))
+            ("host,H", defaultValue(options.hostname))
+            ("task-id,t", defaultValue(options.level))
+            ("team-name", defaultValue(options.teamName))
+            ("password", defaultValue(options.password))
+            ("seed,S", defaultValue(options.seed))
+            ("strategy,s", po::value(&options.strategyString))
+            ;
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).
+            options(description). run(), vm);
+    po::notify(vm);
+
+
+    if (isHelpRequested) {
+        std::cerr << description << std::endl;
+        exit(1);
+    }
+
+    return options;
+}
+
+int main(int argc, const char* argv[]) {
+    Options options = parseOptions(argc, argv);
+    if (options.seed == 0) {
+        options.seed = std::random_device{}();
+    }
+
+    try {
+        platform_dep::enable_socket _;
+        std::mt19937 rng{options.seed};
+
+        using Solver = GenericSolver<ChoosingStrategy>;
+        std::unique_ptr<Solver> solver;
+        if (options.strategyString != "") {
+            solver = std::make_unique<Solver>(
+                            parseStrategy(options.strategyString, rng));
+        } else {
+            std::cerr << "No strategy is chosen!" << std::endl;
+            return 2;
+        }
+        client<Solver> mazeClient{options.hostname.c_str(), options.port,
+                    options.teamName.c_str(), options.password.c_str(),
+                    options.level, std::move(*solver)};
+        mazeClient.run();
+    } catch (std::exception& error) {
+        std::cerr << "Exception thrown: " << error.what() << std::endl;
+    }
+}
